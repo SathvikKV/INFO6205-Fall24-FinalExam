a09355facdf412a1c1f5dac09655365aedfa8845
SathvikKV <vadavatha.s@northeastern.edu>
2024-12-09 15:31:08 -0500

    Final Project Submission
    9/9 Test Cases Passed

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -11,7 +11,15 @@ public class Fibonacci {
      * else <code>fibN(n-1)</code> + <code>fibN(n-2)</code>.
      */
     public long fibN(int n) {
-        return -1L;
+
+        if (n < 0 || n > 92) {
+            return -1;
+        }
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        memo[n] = fibN(n - 1) + fibN(n - 2);
+        return memo[n];
     }
 
     /**
@@ -21,11 +29,28 @@ public class Fibonacci {
      * @return the sum of the Fibonacci numbers 0 through n.
      */
     public long sumN(int n) {
-         return -1L;
+        if (n < 0 || n > 92) {
+            return -1;
+        }
+        long sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += fibN(i);
+        }
+        return sum;
     }
 
     public Fibonacci() {
+
+        memo[0] = 0;
+        memo[1] = 1;
+
+        for (int i = 2; i < memo.length; i++) {
+            memo[i] = -1;
+        }
+
+
     }
 
     private final long[] memo = new long[93];
 }
+
